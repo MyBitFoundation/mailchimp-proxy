8738a4f674ddcda0f9bb9001811460d6db3a91b3
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2018-10-16 15:01:23 +0000

    Refactored to receive a list and an email, valid as per env variables

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
 const express = require('express');
 const Mailchimp = require('mailchimp-api-v3');
+const validator = require("email-validator");
+
 require('dotenv').config();
 
 const mailchimpAPIKey = process.env.MAILCHIMP_API_KEY;
-const listId = process.env.MAILCHIMP_LIST_ID;
+
+const [ MAILCHIMP_MYBIT_GENERAL_SUBSCRIPTIONS_LIST_ID ] = process.env;
+
+const subscriptionsIds = {
+  general: MAILCHIMP_MYBIT_GENERAL_SUBSCRIPTIONS_LIST_ID,
+  ddf: MAILCHIMP_MYBIT_GENERAL_SUBSCRIPTIONS_LIST_ID,
+  newsletter: MAILCHIMP_MYBIT_GENERAL_SUBSCRIPTIONS_LIST_ID,
+  tokensale: MAILCHIMP_MYBIT_GENERAL_SUBSCRIPTIONS_LIST_ID
+}
+
+const availableSubscriptionLists = Object.keys(subscriptionsIds).join(' ')
+
+const errorMessages = {
+  NO_EMAIL: 'Please provide an email',
+  INVALID_EMAIL: 'Please provide a valid email',
+  NO_LIST: 'Please provide a list',
+  INVALID_LIST: `Please a valid list to subscribe to. Valid ones are ${availableSubscriptionLists}`
+}
 
 const app = express();
 app.use(express.json());
 const mailchimp = new Mailchimp(mailchimpAPIKey);
 
 app.post('/api/member', (req, res) => {
-  console.log('Calling ID', listId)
-  mailchimp.post(`/lists/${listId}/members`, req.body)
+  const [ list, email ] = req.body;
+  
+  !email && res.send(errorMessages.NO_EMAIL)
+  !list && res.send(errorMessages.NO_LIST)
+  !validator.validate("test@email.com") && res.send(errorMessages.INVALID_EMAIL)
+  
+  const listId = subscriptionsIds[list]
+  
+  !listId && res.send(errorMessages.INVALID_LIST)
+  
+  console.log(`📩 Trying to subscribe ${email} to our ${list} list`);
+  
+  mailchimp.post(`/lists/${listId}/members`, { 
+    email_address: email,
+    status: 'pending' // Always prompt users to confirm subscription
+  })
   .then(function(results){
+    console.log(`💌 Succeded to subscribe ${email} to our ${list} list`);
     res.send(results);
   })
   .catch(function(err){
+    console.log(`📨 Failed to subscribe ${email} to our ${list} list`);
     res.send(err);
   });
   
 });
 
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log(`Express app listening on port ${port}`);
